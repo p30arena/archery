fe8aff572fe35e3b717329ead21d6841e187e8b1
p30arena <p30arena@gmail.com>
2014-03-12 21:42:05 +0330

    FRESH

---
+++ Assets/ArrowMechanics.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowMechanics : MonoBehaviour {
+
+
+	private Vector3 mouseDownPos = Vector3.zero;
+	private Vector3 mouseUpPos = Vector3.zero;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// MOUSE --test
+
+		if(Input.GetMouseButtonDown(0))
+			mouseDownPos = Input.mousePosition;
+		if(Input.GetMouseButtonUp(0))
+			mouseUpPos = Input.mousePosition;
+
+		Debug.Log(mouseDownPos-mouseUpPos);
+
+
+		// TOUCH --development
+
+		if(Input.touchCount != 1)
+			return;
+		TouchPhase phase = Input.GetTouch(0).phase;
+		if(phase == TouchPhase.Began)
+			Debug.Log("TOUCH");
+		if(phase == TouchPhase.Ended || phase == TouchPhase.Canceled)
+			Debug.Log("RELEASE");
+	}
+}
